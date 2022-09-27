1c780a2ff3282372d8a0d06d2546f929d0d63ec2
AnastasiaTrofimova <114457110+AnastasiaTrofimova@users.noreply.github.com>
2022-09-27 07:44:25 +0500

    Add files via upload

---
+++ Трофимова Анастасия 132 группа/трофимова140922.py
@@ -0,0 +1,35 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/student/Desktop/Трофимова Анастасия 132 группа/трофимова.py 
+p=8
+N8=434
+N10=11
+>>> oct(434)
+'0o662'
+>>> oct(11)
+'0o13'
+>>> 
+ RESTART: C:/Users/student/Desktop/Трофимова Анастасия 132 группа/трофимова.py 
+p=1
+N1=d
+Traceback (most recent call last):
+  File "C:/Users/student/Desktop/Трофимова Анастасия 132 группа/трофимова.py", line 2, in <module>
+    Np=int(input(f"N{p}="))
+ValueError: invalid literal for int() with base 10: 'd'
+>>> 
+ RESTART: C:/Users/student/Desktop/Трофимова Анастасия 132 группа/трофимова.py 
+p=8
+N8=1
+N10=1
+>>> 
+ RESTART: C:/Users/student/Desktop/Трофимова Анастасия 132 группа/трофимова.py 
+p=8
+N8=9
+N10=9
+>>> 
+ RESTART: C:/Users/student/Desktop/Трофимова Анастасия 132 группа/трофимова.py 
+p=8
+N8=123
+N10=6
+>>> 
