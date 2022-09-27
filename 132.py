b4ed075911fe0bf80d52c558e439d757a8e04b5f
AnastasiaTrofimova <114457110+AnastasiaTrofimova@users.noreply.github.com>
2022-09-27 07:56:06 +0500

    Add files via upload

---
+++ 132.py
@@ -0,0 +1,7 @@
+abc=list('abwgdevzijklmnoprstufhcqx')
+print(abc)
+
+morze='.- -.... .-- --. -.. . ...- --.. .. .--- -.- .-.. -- - --- .--. .-. ... - ..- ..-. .... -.-. ---. ---- --.- .--.- -.-- -..- ..-.. ..-- .-.-'
+m=morze.split()
+print(m)
+print(m[0]) 
