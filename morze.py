88bcfd25e2bd1fa790b9ce5017e64f04e2d398a8
Трофимова Анастасия 132 гр <114457110+AnastasiaTrofimova@users.noreply.github.com>
2022-09-28 07:48:40 +0500

    Add files via upload

---
+++ morze.py
@@ -0,0 +1,15 @@
+abc=list('abwgdevzijklmnoprstufhcqx')
+print(abc)
+
+morze='.- -.... .-- --. -.. . ...- --.. .. .--- -.- .-.. -- - --- .--. .-. ... - ..- ..-. .... -.-. ---. ---- --.- .--.- -.-- -..- ..-.. ..-- .-.-'
+m=morze.split()
+print(m)
+print(m[0])
+#перебор посимвольно текста for
+#искали этот символ в алфавите по индекси и запоминали номер.index
+#по этому номеру дергали элемент азбуки морзе [nomer]
+text=input(...)
+for i in text:
+    num=abc.index(1)
+    itog=itog+abcm[num]
+print(itog)
